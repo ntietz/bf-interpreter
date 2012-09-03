57732d7ff113aae8abd4819dc84d28ffbda20259
Nicole Tietz-Sokolskaya <me@ntietz.com>
2012-09-02 23:32:54 -0400

    Added tests & functionality for < and >

--- src/com/ntietz/bf/interpreter/Interpreter.java
+++ src/com/ntietz/bf/interpreter/Interpreter.java
@@ -1,8 +1,11 @@
-package com.ntietz.bf.intepreter;
+package com.ntietz.bf.interpreter;
+
+import java.util.*;
 
 public class Interpreter
 {
     private String program;
+    private String output;
     private List<Integer> data;
     private int dataPointer;
     private int instructionPointer;
@@ -10,9 +13,51 @@ public class Interpreter
     public Interpreter()
     {
         program = "";
+        output = "";
         data = new ArrayList<Integer>();
         dataPointer = 0;
         instructionPointer = 0;
+    }
+
+    public void load(String program)
+    {
+        this.program = program;
+    }
+
+    public void run()
+    {
+        while (instructionPointer < program.length())
+        {
+            char command = program.charAt(instructionPointer);
+
+            switch (command)
+            {
+                case '>' :
+                    ++dataPointer;
+                    break;
+
+                case '<' :
+                    --dataPointer;
+                    break;
+            }
+
+            ++instructionPointer;
+        }
+    }
+
+    public String output()
+    {
+        return output;
+    }
+
+    public int dataPointer()
+    {
+        return dataPointer;
+    }
+
+    public int instructionPointer()
+    {
+        return instructionPointer;
     }
 }
 
