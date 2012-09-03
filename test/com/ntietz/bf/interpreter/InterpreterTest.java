7d06b26f501a34feed745899253332cb27c715ce
Nicholas Tietz <ntietz@gmail.com>
2012-09-02 23:32:54 -0400

    Added tests & functionality for < and >

--- test/com/ntietz/bf/interpreter/InterpreterTest.java
+++ test/com/ntietz/bf/interpreter/InterpreterTest.java
@@ -6,12 +6,6 @@ import static org.junit.Assert.*;
 public class InterpreterTest
 {
     @Test
-    public void test()
-    {
-        fail("A simple test failure");
-    }
-
-    @Test
     public void testBadInput()
     {
         String program = "hello there!";
@@ -21,6 +15,44 @@ public class InterpreterTest
         interpreter.run();
 
         assertEquals("Program should produce no output", interpreter.output(), "");
+    }
+
+    @Test
+    public void testInitialPointers()
+    {
+        String program = "foo bar!";
+
+        Interpreter interpreter = new Interpreter();
+        interpreter.load(program);
+        
+        assertEquals("Data pointer should start at 0", 0, interpreter.dataPointer());
+        assertEquals("Instruction pointer should start at 0", 0, interpreter.instructionPointer());
+    }
+
+    @Test
+    public void testIncrementDataPointer()
+    {
+        String program = "  >  ";
+
+        Interpreter interpreter = new Interpreter();
+        interpreter.load(program);
+        interpreter.run();
+
+        assertEquals("Data pointer should be at 1", 1, interpreter.dataPointer());
+        assertTrue("Instruction pointer should have moved", interpreter.instructionPointer() > 0);
+    }
+
+    @Test
+    public void testDecrementDataPointer()
+    {
+        String program = ">><>";
+
+        Interpreter interpreter = new Interpreter();
+        interpreter.load(program);
+        interpreter.run();
+
+        assertEquals("Data pointer should be at 2", 2, interpreter.dataPointer());
+        assertTrue("Instruction pointer should have moved", interpreter.instructionPointer() > 0);
     }
 }
 
