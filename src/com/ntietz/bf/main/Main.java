c1f053632f54dda182c28825d6760cb77e344670
Nicholas Tietz <ntietz@gmail.com>
2012-09-02 23:21:58 -0400

    Set up project structure and added initial test.

---
+++ src/com/ntietz/bf/main/Main.java
@@ -0,0 +1,9 @@
+package com.ntietz.bf.main;
+
+public class Main
+{
+    public static void main(String... args)
+    {
+        System.out.println("Starting the brainfuck interpreter...");
+    }
+}
